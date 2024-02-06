75547aebfd7044590df063376a0b7c260ca883a6
戸部翔太 <tobeshouta@Intel.local>
2024-02-06 20:02:43 +0900

    [add] enable to proc '%x'

--- ft_printf.c
+++ ft_printf.c
@@ -20,6 +20,25 @@ int	ft_printf(const char *str, ...)
 	return (result_length);
 }
 
+static int	ft_putchar(char c)
+{
+	return (write(STDOUT_FILENO, &c, 1));
+}
+
+int	ft_puthex(unsigned int hex)
+{
+	int	len;
+
+	len = 0;
+	if (hex >= 16)
+	{
+		len += ft_puthex(hex / 16);
+		len += ft_puthex(hex % 16);
+	}
+	else
+		len += ft_putchar("0123456789abcdef"[hex]);
+	return (len);
+}
 
 int	sort_format(const char format, va_list *data)
 {
@@ -30,20 +49,12 @@ int	sort_format(const char format, va_list *data)
 		len += ft_putchar(va_arg(*data, int));
 	if (format == 's')
 		len += ft_putstr(va_arg(*data, char *));
-	if (format == 'p')
-		len += ft_putptr(va_arg(*data, uintptr_t));
 	if (format == 'd')
 		len += ft_putnbr(va_arg(*data, int));
 	if (format == 'i')
 		len += ft_putnbr(va_arg(*data, int));
-	if (format == 'u')
-		len += ft_putunbr(va_arg(*data, unsigned int));
 	if (format == 'x')
-		len += ft_lower_puthex(va_arg(*data, unsigned int));
-	if (format == 'X')
-		len += ft_upper_puthex(va_arg(*data, unsigned int));
-	if (format == 'b')
-		len += ft_putbinary(va_arg(*data, unsigned int));
+		len += ft_puthex(va_arg(*data, unsigned int));
 	if (format == '%')
 		len += write(STDOUT_FILENO, "%", 1);
 	return (len);
