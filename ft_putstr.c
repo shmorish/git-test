54de03246784a4855d597137a624b6358b8ec527
戸部翔太 <tobeshouta@Intel.local>
2024-02-06 18:57:38 +0900

    [add] ft_putstr();

---
+++ ft_putstr.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toshota <toshota@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 18:57:01 by toshota           #+#    #+#             */
+/*   Updated: 2024/02/06 18:57:30 by toshota          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+size_t	ft_strlen(char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+int	ft_putstr(char *str)
+{
+	return (write(STDOUT_FILENO, str, ft_strlen(str)));
+}
